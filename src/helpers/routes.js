cb2288be2a2ee1d79745823526b94bdb24195923
arthurstx <arthurteixeira.git@gmail.com>
2026-03-04 22:14:56 -0300

    set Allow-credentials : true

--- src/helpers/routes.js
+++ src/helpers/routes.js
@@ -2,6 +2,7 @@ import { handleError } from './error.js'
 
 const corsHeaders = {
 	'Access-Control-Allow-Origin': 'http://localhost:5173',
+	'Access-Control-Allow-Credentials': true,
 	'Access-Control-Allow-Methods': 'GET,HEAD,POST,PATCH,DELETE,OPTIONS',
 	'Access-Control-Allow-Headers': 'Content-Type, Authorization',
 	'Access-Control-Max-Age': '86400',
