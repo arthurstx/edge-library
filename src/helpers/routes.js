cc6ad140933e324370f22e15e94b705d7e2e8a0b
arthurstx <arthurteixeira.git@gmail.com>
2026-03-28 21:18:19 -0300

    chore: edit allow-origin in cors

--- src/helpers/routes.js
+++ src/helpers/routes.js
@@ -1,7 +1,7 @@
 import { handleError } from './error.js'
 
 const corsHeaders = {
-	'Access-Control-Allow-Origin': 'http://localhost:5173',
+	'Access-Control-Allow-Origin': 'https://front-edge-library.vercel.app',
 	'Access-Control-Allow-Credentials': true,
 	'Access-Control-Allow-Methods': 'GET,HEAD,POST,PATCH,DELETE,OPTIONS',
 	'Access-Control-Allow-Headers': 'Content-Type, Authorization',
